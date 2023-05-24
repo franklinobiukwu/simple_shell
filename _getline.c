15f6ebe729a2391d691919827ad8f7bb9f94b74d
Edeh sharon <Pearllizzy196@gmail.com>
2023-05-24 05:14:02 -0700

    betty

--- _getline.c
+++ _getline.c
@@ -1,14 +1,61 @@
 #include "shell.h"
 
-void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void assign_linptr(char **linptr, size_t *n, char *buffer, size_t b);
-ssize_t my_getline(char **lineptr, size_t *n, FILE *stream);
+/**
+ * my_getline - reas input from stream
+ * @lineptr: buffer to store input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: number of bytes read
+ */
+ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
+{
+static ssize_t input;
+ssize_t ret;
+char c = 'x', *buffer;
+int r;
+
+if (input == 0)
+fflush(stream);
+else
+return (-1);
+input = 0;
+buffer = malloc(sizeof(char) * 120);
+if (!buffer)
+return (-1);
+while (c != '\n')
+{
+r = read(STDIN_FILENO, &c, 1);
+if (r == -1 || (r == 0 && input == 0))
+{
+free(buffer);
+return (-1);
+}
+if (r == 0 && input != 0)
+{
+input++;
+break;
+}
+if (input >= 120)
+buffer = my_realloc(buffer, input, input + 1);
+
+buffer[input] = c;
+input++;
+}
+buffer[input] = '\0';
+
+assign_linptr(lineptr, n, buffer, input);
+ret = input;
+if (r != 0)
+input = 0;
+return (ret);
+}
 
 /**
  * my_realloc - Reallocate memeroy block
  * @ptr: pointer to a memory perviously allocated
  * @old_size: size in bytes of the allocated space for ptr
  * @new_size: size in bytes for new memory
+ * Return: memory
  */
 
 void *my_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -66,59 +113,9 @@ void assign_linptr(char **linptr, size_t *n, char *buffer, size_t b)
 			*n = 120;
 		*linptr = buffer;
 	}
-	else 
+	else
 	{
 		my_strcpy(*linptr, buffer);
 		free(buffer);
 	}
-}
-
-/**
- * my_getlin - reas input from stream
- * @lineptr: buffer to store input
- * @n: size of lineptr
- * @stream: stream to read from
- * Return: number of bytes read
- */
-ssize_t my_getline(char **lineptr, size_t *n, FILE *stream)
-{
-	static ssize_t input;
-	ssize_t ret;
-	char c = 'x', *buffer;
-	int r;
-
-	if (input == 0)
-		fflush(stream);
-	else
-		return (-1);
-	input = 0;
-	buffer = malloc(sizeof(char) * 120);
-	if (!buffer)
-		return (-1);
-	while (c != '\n')
-	{
-		r = read(STDIN_FILENO, &c, 1);
-		if (r == -1 || (r == 0 && input == 0))
-		{
-			free(buffer);
-			return (-1);
-		}
-		if (r == 0 && input != 0)
-		{
-			input++;
-			break;
-		}
-		if (input >= 120)
-			buffer = my_realloc(buffer, input, input + 1);
-
-		buffer[input] = c;
-		input++;
-	}
-	buffer[input] = '\0';
-
-	assign_linptr(lineptr, n, buffer, input);
-	ret = input;
-	if (r != 0)
-		input = 0;
-	return (ret);
 }
